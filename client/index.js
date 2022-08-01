f31443918ba538b343125868667bc1803b9bfe45
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-08-01 01:53:09 +0000

    this was pretty quick, this is cool tho, might focus on improving it

--- client/index.js
+++ client/index.js
@@ -1,12 +1,62 @@
-var socket = null;
-const theURL = document.getElementById('websockURL');
+var socket = new WebSocket('ws://sancopublic.ddns.net:6543/');
+
+socket.addEventListener('message', (resp) => {
+    console.log("got data");
+    if(resp.data == "Connected!")
+    {
+        document.getElementById('wsStatus').innerText = "Connected";
+    }
+    else
+    {
+        var jsonData = JSON.parse(resp.data);
+        console.table(jsonData);
+
+        addMessage(jsonData.user, jsonData.content);
+    }
+});
 
 function onLoad()
 {
+    var the = localStorage.getItem('wsName');
+    var daData = {
+        'type': 'userName',
+        'content': ((the != null || undefined) ? the : "guest")
+    };
+    socket.send(JSON.stringify(daData, null, 2));
 
+    document.getElementById('wsName').value = the;
 }
 
-function startConnection()
+function applyName()
 {
-    socket = new WebSocket(theURL.value);
+    localStorage.setItem('wsName', document.getElementById('wsName').value);
+    window.location.reload(true);
+}
+
+function sendMessage()
+{
+    var damsg = document.getElementById('wsMessage').value;
+    var daData = {
+        'type': 'message',
+        'user': localStorage.getItem('wsName'),
+        'content': damsg
+    };
+    socket.send(JSON.stringify(daData, null, 2));
+}
+
+function addMessage(messageAuthor, messageContent)
+{
+    var newMsgItem = document.createElement('div');
+
+    var msgItemAuthor = document.createElement('h2');
+    msgItemAuthor.style.color = 'white';
+    msgItemAuthor.innerText = messageAuthor;
+
+    var msgItemContent = document.createElement('h1');
+    msgItemContent.style.color = 'white';
+    msgItemContent.innerText = messageContent;
+
+    newMsgItem.appendChild(msgItemAuthor);
+    newMsgItem.appendChild(msgItemContent);
+    document.getElementById('messages').appendChild(newMsgItem);
 }
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,31 @@
 import { WebSocketServer } from 'ws';
 
-const port:any = 1337;
+const port:any = 6543;
 const wss = new WebSocketServer(({port: port}));
 
 wss.on('connection', (ws) => {
     console.log('Connection!');
+
+    ws.on('message', (data) => {
+        var jsonData = JSON.parse(data.toString());
+        console.table(jsonData);
+
+        switch(jsonData.type)
+        {
+            case 'message':
+                console.log("trying to send data back");
+                var daData = {
+                    'user': jsonData.user,
+                    'content': jsonData.content
+                };
+                console.table(daData);
+                wss.clients.forEach(client => client.send(JSON.stringify(daData, null, 2)));
+                break;
+            default:
+                //do nothing
+                break;
+        }
+    });
 
     ws.send('Connected!');
 });
